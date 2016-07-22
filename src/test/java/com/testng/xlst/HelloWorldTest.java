56d292a6d3456d48cdefadd8c9647af3c4bb6aeb
shailu2006@gmail.com <shailu2006@gmail.com>
2016-07-22 08:12:40 +0530

    Initial commit

---
+++ src/test/java/com/testng/xlst/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.testng.xlst;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by shailesh on 7/21/2016.
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void helloWorldTest(){
+        Assert.assertTrue(true);
+    }
+}
